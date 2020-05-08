1fc0f09dd315213c8dd87bedd64747a5ac325c02
sangmin802 <56113148+sangmin802@users.noreply.github.com>
2020-05-08 15:30:49 +0900

    gitignore

--- db/db.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql');
-
-module.exports = mysql.createConnection({
-  host : 'localhost',
-  user : 'root',
-  password : '1234',
-  database : 'dnfrankuser'
-})
--- public/js/api.js
+++
@@ -1,2 +0,0 @@
-const api = 'dZSOLkwwB8F1e2ikf9jGNf5mbvdJS6tt';
-export default api;
